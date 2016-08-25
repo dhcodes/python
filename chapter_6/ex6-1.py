248b5a75f79459f79fc43e47ce388a9455f2b130
dhcodes <thor.dh@gmail.com>
2016-08-25 14:44:50 -0500

    first chapter 6 ex.

---
+++ chapter_6/ex6-1.py
@@ -0,0 +1,5 @@
+person = {'first_name':'bob', 'last_name':'barker','city':'los angeles', 'age':92, }
+print(person['first_name'])
+print(person['last_name'])
+print(person['city'])
+print(person['age'])
---
+++ chapter_6/ex6-2.py
@@ -0,0 +1,6 @@
+fav_numbers = {'jim':3, 'ethel': 45, 'tina': 32, 'joey': 20, 'chris': 10,}
+print("Jim's favorite number is: " + str(fav_numbers['jim']))
+print("Ethel's favorite number is: " + str(fav_numbers['ethel']))
+print("Tina's favorite number is: " + str(fav_numbers['tina']))
+print("Joey's favorite number is: " + str(fav_numbers['joey']))
+print("Chris's favorite number is: " + str(fav_numbers['chris']))
